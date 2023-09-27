cce324d02e9d990b8568a955cd295b975c729259
Andrey <a@a.ru>
2023-09-27 16:39:23 +0200

    Добавить router вьюсета review.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import ReviewViewSet
+
 app_name = 'api'
 
 router_v1 = DefaultRouter()
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
