e8ccdba26d38e8582d8062e268045294a268428a
Andrey <a@a.ru>
2023-09-27 16:38:07 +0200

    Добавить сериализатор модели review.

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from reviews.models import Review
+
+
+class GetTitleId:
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        return serializer_field.context['request'].parser_context['kwargs']['title_id']
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    """Сериализатор модели review."""
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=serializers.CurrentUserDefault(),
+        required=False
+    )
+    title = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=GetTitleId()
+    )
+
+    class Meta:
+        model = Review
+        fields = (
+            'id',
+            'text',
+            'author',
+            'score',
+            'pub_date',
+        )
+        read_only_fields = ('title',)
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('title', 'author')
+            )
+        ]
+
+    def validate_score(self, value):
+        if 1 < value > 10:
+            raise serializers.ValidationError(
+                'Оценка должна быть от 1 до 10.'
+            )
+        return value
