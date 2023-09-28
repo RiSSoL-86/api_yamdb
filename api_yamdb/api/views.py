6cab7419d2c68bb80f24f667a48e83caea1d7f28
andre <a@a.ru>
2023-09-28 21:23:00 +0200

    Добавить viewset модели comment.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .serializers import CommentSerializer
+from reviews.models import (
+    Review, Comment, Title, User
+)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_review(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('reviews_id')
+        print(review_id)
+        print(title_id)
+        return get_object_or_404(
+            Review,
+            pk=review_id,
+            title__id=title_id
+        )
+
+    def get_queryset(self):
+        review = self.get_review()
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = self.get_review()
+        serializer.save(
+            review=review,
+            author=self.request.user
+        )
