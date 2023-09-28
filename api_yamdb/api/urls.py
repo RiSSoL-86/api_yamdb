3433bed34fe8864dbcfac1bcfc545bf0140afac2
andre <a@a.ru>
2023-09-28 21:23:59 +0200

    Добавить роутер CommentViewSet.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import CommentViewSet
+
 app_name = 'api'
 
 router_v1 = DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<reviews_id>\d+)/comments',
+    CommentViewSet
+)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
