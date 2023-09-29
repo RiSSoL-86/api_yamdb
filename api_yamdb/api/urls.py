00440623b5702f182f71944f8736716ae9efbd71
Максим Мякотин <bobteen7@gmail.com>
2023-09-29 13:03:52 +0200

    Version 1.0

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import (APIGetToken, APISignup, UsersViewSet)
+
 app_name = 'api'
 
 router_v1 = DefaultRouter()
+router_v1.register(
+    'users',
+    UsersViewSet,
+    basename='users'
+)
 
 urlpatterns = [
+    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
     path('v1/', include(router_v1.urls)),
 ]
